041099166b881a4ea97529df9b8d2d65a277b87d
evgen <dreyko-net@yandex.com>
2026-06-10 10:43:11 +0300

    debug

--- telegram/__init__.py
+++ telegram/__init__.py
@@ -1,12 +1,26 @@
-from aiogram import Bot, Dispatcher
+import logging
+from aiogram import Bot, Dispatcher, BaseMiddleware
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.types import TelegramObject
 
 from telegram.handlers import auth, messages, callbacks
+
+log = logging.getLogger(__name__)
+
+
+class LogAllUpdatesMiddleware(BaseMiddleware):
+    async def __call__(self, handler, event: TelegramObject, data: dict):
+        log.info("[MIDDLEWARE] update type=%s data_keys=%s",
+                 type(event).__name__, list(data.keys()))
+        result = await handler(event, data)
+        return result
 
 
 def create_bot(token: str) -> tuple[Bot, Dispatcher]:
     bot = Bot(token=token)
     dp  = Dispatcher(storage=MemoryStorage())
+
+    dp.update.middleware(LogAllUpdatesMiddleware())
 
     dp.include_router(auth.router)
     dp.include_router(callbacks.router)
